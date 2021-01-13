0a4afb21f1b5575375f730eaeb393b842873a173
kimjinsoo <roorooro9933@gmail.com>
2021-01-13 12:11:39 +0900

    init

---
+++ src/main/java/com/jinsoo/remindjpa/RemindJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jinsoo.remindjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinsoo/remindjpa/RemindJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinsoo.remindjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
